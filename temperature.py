e7da8d0fa8ce27d5f570a6823fc60b6c9288b78e
Esc-04 <escho94@naver.com>
2025-06-09 14:40:26 +0000

    add info and "End"

--- temperature.py
+++ temperature.py
@@ -1,7 +1,8 @@
+#final-exam firstGrade-half
 temp=int(input("input current temperature>>"))
 if temp>=35:
 	print("Turn on air-con")
 else:
-	print("Turn on air-con")
+	print("Turn off air-con")
 
-
+print("End")
